d405d15f4345fae5d8af97e006caded43e3e649a
Sheepy <liammehlex@gmail.com>
2023-12-21 13:42:35 +0100

    idk

--- src/net.rs
+++ src/net.rs
@@ -1,106 +1,32 @@
 use core::slice;
 use std::io::ErrorKind;
 
+use serde::{Serialize, Deserialize};
 use futures::TryFutureExt;
 use tokio::net::TcpStream;
 use tokio::io::{Result, Interest, Error, ErrorKind::{WouldBlock, ConnectionRefused, NotConnected}};
 
 type PacketId = u8;
 
-trait Serialize {
-	fn seriazlie(&self) -> Vec<u8>;
-}
-
-trait Deserialize where Self: Sized {
-	fn deseriazlie(buffer: impl AsRef<[u8]>) -> Option<Self>;
-}
-
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Debug, Clone)]
 struct Login {
 	pub username: String,
 	pub hashed_password: String
 }
 
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Debug, Clone)]
 struct Message {
 	pub from: String,
 	pub to: String,
 	pub message: String
 }
 
-impl Serialize for Login {
-	fn seriazlie(&self) -> Vec<u8> {
-		const SEP: u8 = '|' as u8;
-		const SEP_LEN: usize = 1;
-
-		let mut output = Vec::with_capacity(self.username.len()+SEP_LEN+self.hashed_password.len());
-		output.push(LOGIN_DISCRIMINANT);
-		output.extend_from_slice(self.username.as_bytes());
-		output.push(SEP);
-		output.extend_from_slice(self.hashed_password.as_bytes());
-
-		output
-	}
-}
-
-impl Deserialize for Login {
-	fn deseriazlie(buffer: impl AsRef<[u8]>) -> Option<Self> {
-		let mut parts = buffer.as_ref().split(|c| *c == '|' as u8);
-		let raw_username = parts.next()?;
-		let raw_password = parts.next()?;
-		let username = String::from_utf8(raw_username.to_owned()).ok()?;
-		let password = String::from_utf8(raw_password.to_owned()).ok()?;
-		
-		if let Some(_) = parts.next() { return None }
-
-		Some(Login {
-			username: username,
-			hashed_password: password
-		})
-	}
-}
-
-const LOGIN_DISCRIMINANT: PacketId = 1;
-const MESSAGE_DISCRIMINANT: PacketId = 2;
-const MESSAGE_LOGS_REQUEST_DISCRIMINANT: PacketId = 3;
-const MESSAGE_LOGS_DISCRIMINANT: PacketId = 4;
-
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Debug, Clone)]
 enum Action {
 	Login(Login),
 	Message(Message),
 	MessageLogsRequest,
 	MessageLogs
-}
-
-impl Serialize for Action {
-	fn seriazlie(&self) -> Vec<u8> {
-		match self {
-			Self::Login(x) => x.seriazlie(),
-			Self::Message(_) => todo!(),
-			Self::MessageLogsRequest => todo!(),
-			Self::MessageLogs => todo!()
-		}
-	}
-}
-
-impl Deserialize for Action {
-	fn deseriazlie(buffer: impl AsRef<[u8]>) -> Option<Self> {
-		const ID_LEN:usize = std::mem::size_of::<PacketId>();
-		let buffer = buffer.as_ref();
-		// to_be() does the same as a "from_be", if the machine is big endian, it's a no-op, if it's little endian, the bytes are swapped.
-		// network order is big-endian
-		let variant_id = unsafe { std::mem::transmute::<[u8; ID_LEN], PacketId>(buffer[..ID_LEN].try_into().unwrap()) }.to_be();
-		let payload = &buffer[ID_LEN..];
-
-		match variant_id {
-			x if x == LOGIN_DISCRIMINANT => Login::deseriazlie(payload).map(|x| Self::Login(x)),
-			// x if x == message => {todo!()},
-			// x if x == request_message_log => {todo!()},
-			// x if x == message_log => {todo!()}
-			_ => {todo!()}
-		}
-	}
 }
 
 #[allow(dead_code)]
@@ -189,13 +115,14 @@ macro_rules! read_packet {
 
 #[cfg(test)]
 pub mod tests {
-    use crate::net::{Serialize, Deserialize};
 	#[test]
 	fn login_packet() {
-		use crate::net;
-		let action = net::Action::Login(net::Login {username: "1234".to_owned(), hashed_password: "5678".to_owned()});
-		let packet_data = action.seriazlie();
-		let deserialized_action = net::Action::deseriazlie(packet_data).unwrap();
-		assert_eq!(deserialized_action, action);
+		use crate::net::{Action, Login};
+		use serde_json;
+		let action = Action::Login(Login {
+				username: "me".to_owned(),
+				hashed_password: "my pass".to_owned()
+			});
+		// let serialized_action = serde_json::to_string(&action).unwrap();
 	}
 }
